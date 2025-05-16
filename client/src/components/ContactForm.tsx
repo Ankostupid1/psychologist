bbe6ab2848e919c4dbf668a652303395a9fe5884
Ankostupid1 <kariukiantony@protonmail.com>
2025-05-16 22:17:07 +0300

    on branch master

--- client/src/components/ContactForm.tsx
+++ client/src/components/ContactForm.tsx
@@ -27,7 +27,7 @@ const ContactForm = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://ankostupid.vercel.app/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
